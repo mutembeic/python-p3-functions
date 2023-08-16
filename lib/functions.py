d504d3afe6a25541b7d14b3b8b005a9d5cb4e711
mutembeic <135004069+mutembeic@users.noreply.github.com>
2023-08-16 12:36:02 +0300

    done

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,28 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
-
+    print("Hello, programmer!")
+     
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+    
+greet('Naureen')
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
+greet_with_default("Sunny")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+print (add(2,1))
 
+ 
 def halve(number):
-    pass
+    if not isinstance(number, (int, float)):
+        return None
+
+    return number / 2
+
+   
+ 
